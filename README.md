8aa5749237b9a7815600c68766e67b97bc9c4bed
Sam Kohl <Samkohl@Samkohl.com>
2022-07-08 18:25:41 -0700

    Added db connection / ejs

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Star-Wars-Character-Quotes-App
-An full-stack app built along to https://zellwk.com/blog/crud-express-mongodb/
+A full-stack app built along to https://zellwk.com/blog/crud-express-mongodb/
--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient
 const app = express();
+const connectionString = 'mongodb+srv://skohlcode:eou060Y6Ya3wpGTK@starwarscluster.kdrgp.mongodb.net/?retryWrites=true&w=majority'
+
+MongoClient.connect(connectionString,{ useUnifiedTopology: true })
+.then(client => {
+    console.log('Connected to database');
+    const db = client.db('starwarscluster');
+    const quotesCollection = db.collection('quotes');
+
+    app.set('view engine', 'ejs');
+
+    app.post('/quotes', (req,res) => {
+        quotesCollection.insertOne(req.body)
+        .then(result => {
+            console.log(result);
+            res.redirect('/');
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.get('/', (req, res) => {
+        db.collection('quotes').find().toArray()
+        .then(results => {
+            console.log(results);
+            res.render('index.ejs', { quotes: results});
+        })
+        .catch(error => console.error(error))
+    
+        
+    })
+
+
+}).catch(error => console.error(error))
+
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
 
 app.listen(3000, function(){
-    console.log('Listening on 3000')
+    console.log('Listening on 3000');
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+
+
